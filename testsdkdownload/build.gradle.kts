3d703b37b7ce1ef3260bb45316ee2e08f0cfbf36
Jarvis Semou <mouse.jiangwei@gmail.com>
2022-04-08 17:12:28 +0800

    使用 gradle 原生 api 打包依赖包

--- testsdkdownload/build.gradle.kts
+++ testsdkdownload/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
+    // 文件下载插件
+    id("de.undercouch.download")
 }
 
 android {
@@ -32,37 +34,51 @@ android {
         jvmTarget = "1.8"
     }
 
-    task<Exec>(name = "downLoadSdk") {
-        workingDir = File("./")
+    val version = "2.0.8.4"
+    val sdkName = "trtc_engine.zip"
+    val downloadedSdkFile = File("$buildDir/ti-sdk/$sdkName")
 
-        commandLine = listOf(
-            "python",
-            "download_native_sdk.py",
-            "--sdk_download_url",
-            "https://artifact-master.zego.cloud/generic/rtc/public/native/ZegoExpressVideo/android/ZegoExpressVideo-android-shared-java.zip?version=2.17.1.20322",
-            "--sdk_zip_root_folder",
-            "release/Library"
-        )
+    task<de.undercouch.gradle.tasks.download.Download>("downLoadSdk") {
+        src("https://tinet-sdk-release.s3.cn-north-1.amazonaws.com.cn/trtc-engine/sdk/android/v$version/$sdkName")
+        dest(downloadedSdkFile)
+        overwrite(true)
+        onlyIfModified(true)
     }
 
-    tasks.withType(JavaCompile::class.java) {
+    task<Copy>("copySo"){
         dependsOn("downLoadSdk")
+        from(zipTree(downloadedSdkFile))
+        include("arm*/**")
+        into("${project.projectDir}/src/main/jniLibs")
+    }
+
+    task<Copy>("initSdk") {
+        dependsOn("copySo")
+        from(zipTree(downloadedSdkFile))
+        include("*.jar")
+        into("${project.projectDir}/libs")
+    }
+
+
+
+    tasks.withType(JavaCompile::class.java) {
+        dependsOn("initSdk")
     }
 }
 
 afterEvaluate {
-    publishing{
-        publications{
-            create("_TestSdkDownload-release_",MavenPublication::class.java){
-                groupId="io.github.JarvisSemou"
-                artifactId="TiAndroidTools"
+    publishing {
+        publications {
+            create("_TestSdkDownload-release_", MavenPublication::class.java) {
+                groupId = "io.github.JarvisSemou"
+                artifactId = "TiAndroidTools"
                 version = "v1.0.0"
                 from(components["release"])
             }
 
-            create("_TiAndroidTools-debug_",MavenPublication::class.java){
-                groupId="io.github.JarvisSemou"
-                artifactId="TiAndroidTools"
+            create("_TiAndroidTools-debug_", MavenPublication::class.java) {
+                groupId = "io.github.JarvisSemou"
+                artifactId = "TiAndroidTools"
                 version = "v1.0.0-SNAPSHOT"
                 from(components["debug"])
             }
@@ -71,14 +87,6 @@ afterEvaluate {
 }
 
 dependencies {
-
-//    implementation("androidx.core:core-ktx:1.7.0")
-//    implementation("androidx.appcompat:appcompat:1.4.1")
-//    implementation("com.google.android.material:material:1.5.0")
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-
     api(
         fileTree(
             mapOf(
